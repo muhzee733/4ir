b5fe485ce6272c580301c012a6c540495bb24079
shahbaz <shahbaznadeem654@gmail.com>
2024-11-26 11:32:08 +0500

    footer create and basic setup

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,13 @@
 import Navbar from './Navbar/Header'
 import Footer from './Navbar/Footer'
- 
+
 export default function Layout({ children }) {
   return (
     <>
       <Navbar />
-      <main>{children}</main>
+      <main style={{ minHeight: '85vh' }}>
+        {children}
+      </main>
       <Footer />
     </>
   )
