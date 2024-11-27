7a87962b99b14639dd124aa68fc01aa83a557be4
shahbaz <shahbaznadeem654@gmail.com>
2024-11-27 10:50:48 +0500

    Footer Finalized

--- src/components/Navbar/Header.jsx
+++ src/components/Navbar/Header.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Header = () => {
+  const [isSticky, setIsSticky] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 500) {
+        setIsSticky(true);
+      } else {
+        setIsSticky(false);
+      }
+    };
+
+    // handleScroll()
+
+    if (typeof window !== "undefined") {
+      window.addEventListener("scroll", handleScroll);
+    }
+
+    return () => {
+      if (typeof window !== "undefined") {
+        window.removeEventListener("scroll", handleScroll);
+      }
+    };
+  }, []);
+
+
   return (
-    <div>Header</div>
+    <div className={isSticky ? "position-sticky" : ""}>
+      <h1>4ir</h1>
+      <h2>4ir</h2>
+      <h3>4ir</h3>
+      <h4>4ir</h4>
+      <h5>4ir</h5>
+      <h6>4ir</h6>
+      <p>Next.js Application</p>
+    </div>
   )
 }
 
--- src/components/layout.js
+++ src/components/layout.js
@@ -5,7 +5,7 @@ export default function Layout({ children }) {
   return (
     <>
       <Navbar />
-      <main style={{ minHeight: '85vh' }}>
+      <main style={{ minHeight: '200vh' }}>
         {children}
       </main>
       <Footer />
