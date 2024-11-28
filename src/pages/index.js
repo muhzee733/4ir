7f5047d7b8aaa57531bcefa32752f1e4754cd163
basit <cool.saa733@gmail.com>
2024-11-28 17:42:55 +0500

    banner

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import H1 from "@/components/typography/H1";
 import H2 from "@/components/typography/H2";
 import Robot from "@/images/landing/robot.png";
 import Image from "next/image";
-import "./index.css"
+import "./index.css";
 
 const Index = () => {
   return (
@@ -15,34 +15,29 @@ const Index = () => {
       </Head>
       <Layout>
         <section className="setbackground">
-          <div className="container-fluid">
-            <div className="row ">
-              <div className="col-lg-6 col-md-12 col-sm-12">
-                <div className=" text-center p-5 ">
+          <div className="main-wrapper">
+            <div className="container-fluid">
+              <div className="row " style={{ minHeight: "100vh" }}>
+                <div className="col-lg-6 col-md-12 col-sm-12 d-flex justify-content-center align-items-center flex-column">
                   <H2 title="FOURTH INDUSTRIAL REVOLUTION INC" />
-                  <H1 breakpoint={true}
+                  <H1
+                    breakpoint={true}
                     spantitle="EMPOWER YOUR BUSINESS"
-                    title="WITH AI-DRIVEN SUCCESS" />
+                    title="WITH AI-DRIVEN SUCCESS"
+                  />
                   <button className="btn-2 mt-4">Book a Demo</button>
                 </div>
-              </div>
 
-              <div className="col-lg-6 text-white d-none d-lg-block d-xl-block ">
-                <div className="d-flex justify-content-center">
-                  <div className="landing-robot d-flex justify-content-end align-items-end">
-                    <Image
-                      src={Robot}
-                      alt="robot"
-                    />
+                <div className="col-lg-6 d-flex align-items-end justify-content-end">
+                  <div className="landing-robot">
+                    <figure className="m-0  text-end">
+                      <Image src={Robot} alt="robot"  width={400}/>
+                    </figure>
                   </div>
                 </div>
               </div>
-
             </div>
-
-
           </div>
-
         </section>
       </Layout>
     </>
