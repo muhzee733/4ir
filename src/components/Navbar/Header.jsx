ba91d0511476272e9252c45b0f08dc05c72ad991
basit <cool.saa733@gmail.com>
2024-11-28 12:45:41 +0500

    white karahi

--- src/components/Navbar/Header.jsx
+++ src/components/Navbar/Header.jsx
@@ -3,228 +3,170 @@ import MainLogo from "../../../public/assets/images/4IRLogomain.png";
 import Image from "next/image";
 import { FaCircle } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
+import { FiAlignJustify } from "react-icons/fi";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
 const Header = () => {
   const router = useRouter();
   const isActive = (path) => router.pathname === path;
-  const [isSticky, setIsSticky] = useState(false);
   const [isCollapsed, setIsCollapsed] = useState(true);
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   const toggleSidebar = () => {
     setIsCollapsed(!isCollapsed);
   };
 
-  const toggleDropdown = () => {
-    setIsDropdownOpen(!isDropdownOpen);
-  };
-
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 100) {
-        setIsSticky(true);
-      } else {
-        setIsSticky(false);
-      }
-    };
-
-    if (typeof window !== "undefined") {
-      window.addEventListener("scroll", handleScroll);
-    }
-
-    return () => {
-      if (typeof window !== "undefined") {
-        window.removeEventListener("scroll", handleScroll);
-      }
-    };
-  }, []);
-
   return (
     <>
-      <div className={isSticky ? "sticky-top" : ""}>
-        <header className="bg-dark d-none d-xl-block">
-          <div className="container">
-            <div className="row header-main-row">
-              <div className="col-lg-2 d-flex justify-content-center align-items-center">
-                <Link href="/">
-                  <Image src={MainLogo} className="img-fluid" alt="Logo" />
-                </Link>
-              </div>
-              <div className="col-lg-8">
-                <ul className="d-flex align-items-center justify-content-around list-unstyled h-100 fs-5">
-                  <li>
-                    <Link
-                      href="/"
-                      className={`header-nav-li text-decoration-none ${isActive("/") ? "active-link" : ""
-                        }`}
+      <header className="bg-dark">
+        <div className="container">
+          <div className="row header-main-row">
+            <div className="col-lg-2 col-md-6 col col-sm-4 d-flex justify-content-left align-items-center">
+              <Link href="/">
+                <Image src={MainLogo} className="img-fluid" alt="Logo" />
+              </Link>
+            </div>
+            <div className="col-lg-8 custom-hide">
+              <ul className="d-flex align-items-center justify-content-around list-unstyled h-100 fs-5">
+                <li>
+                  <Link
+                    href="/"
+                    className={`header-nav-li text-decoration-none ${
+                      isActive("/") ? "active-link" : ""
+                    }`}
+                  >
+                    <span
+                      className={`header-nav-li text-decoration-none ${
+                        isActive("/")
+                          ? "header-nav-li-span1"
+                          : "header-nav-li-span"
+                      }`}
                     >
-                      <span className={`header-nav-li text-decoration-none ${isActive("/") ? "header-nav-li-span1" : "header-nav-li-span"
-                        }`}>
-                        <FaCircle size={7} />
-                      </span>
-                      Home
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      href="/about-us"
-                      className={`header-nav-li text-decoration-none ${isActive("/about-us") ? "active-link" : ""
-                        }`}
+                      <FaCircle size={7} />
+                    </span>
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/about-us"
+                    className={`header-nav-li text-decoration-none ${
+                      isActive("/about-us") ? "active-link" : ""
+                    }`}
+                  >
+                    <span
+                      className={`header-nav-li text-decoration-none ${
+                        isActive("/about-us")
+                          ? "header-nav-li-span1"
+                          : "header-nav-li-span"
+                      }`}
                     >
-                      <span className={`header-nav-li text-decoration-none ${isActive("/about-us") ? "header-nav-li-span1" : "header-nav-li-span"
-                        }`}>
-                        <FaCircle size={7} />
-                      </span>
-                      About Us
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      href="/services"
-                      className={`header-nav-li text-decoration-none ${isActive("/services") ? "active-link" : ""
-                        }`}
+                      <FaCircle size={7} />
+                    </span>
+                    About Us
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/services"
+                    className={`header-nav-li text-decoration-none ${
+                      isActive("/services") ? "active-link" : ""
+                    }`}
+                  >
+                    <span
+                      className={`header-nav-li text-decoration-none ${
+                        isActive("/services")
+                          ? "header-nav-li-span1"
+                          : "header-nav-li-span"
+                      }`}
                     >
-                      <span className={`header-nav-li text-decoration-none ${isActive("/services") ? "header-nav-li-span1" : "header-nav-li-span"
-                        }`}>
-                        <FaCircle size={7} />
-                      </span>
-                      Services
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      href="/blog"
-                      className={`header-nav-li text-decoration-none ${isActive("/blog") ? "active-link" : ""
-                        }`}
+                      <FaCircle size={7} />
+                    </span>
+                    Services
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/blog"
+                    className={`header-nav-li text-decoration-none ${
+                      isActive("/blog") ? "active-link" : ""
+                    }`}
+                  >
+                    <span
+                      className={`header-nav-li text-decoration-none ${
+                        isActive("/blog")
+                          ? "header-nav-li-span1"
+                          : "header-nav-li-span"
+                      }`}
                     >
-                      <span className={`header-nav-li text-decoration-none ${isActive("/blog") ? "header-nav-li-span1" : "header-nav-li-span"
-                        }`}>
-                        <FaCircle size={7} />
-                      </span>
-                      Blog
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      href="/contact-us"
-                      className={`header-nav-li text-decoration-none ${isActive("/contact-us") ? "active-link" : ""
-                        }`}
+                      <FaCircle size={7} />
+                    </span>
+                    Blog
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/contact-us"
+                    className={`header-nav-li text-decoration-none ${
+                      isActive("/contact-us") ? "active-link" : ""
+                    }`}
+                  >
+                    <span
+                      className={`header-nav-li text-decoration-none ${
+                        isActive("/contact-us")
+                          ? "header-nav-li-span1"
+                          : "header-nav-li-span"
+                      }`}
                     >
-                      <span className={`header-nav-li text-decoration-none ${isActive("/contact-us") ? "header-nav-li-span1" : "header-nav-li-span"
-                        }`}>
-                        <FaCircle size={7} />
-                      </span>
-                      Contact Us
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-              <div className="col-lg-2 d-flex justify-content-end align-items-center">
-                <button className="btn-1">Book a Demo</button>
-              </div>
+                      <FaCircle size={7} />
+                    </span>
+                    Contact Us
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className="col-lg-2 col-md-6 d-flex justify-content-end align-items-center custom-hide">
+              <button className="btn-1">Book a Demo</button>
+            </div>
+            <div
+              className="col-md-6 custom-show col-sm-8 col d-flex align-items-center justify-content-end"
+              onClick={() => toggleSidebar()}
+            >
+              <FiAlignJustify
+                className="text-white"
+                style={{ fontSize: "30px" }}
+              />
             </div>
           </div>
-        </header>
-      </div>
-
-
-      {/* side bar nav */}
-      <div className="headersidebar-main">
-        <div className="d-flex justify-content-between">
-          <div>
-            <Link href="/">
-              <Image src={MainLogo} className="img-fluid" alt="Logo" />
-            </Link>
-          </div>
-
-          <button
-            className="btn btn-info"
-            onClick={toggleSidebar}
-            title={isCollapsed ? "Expand" : "Collapse"}
-          >
-            {/* {isCollapsed ? "☰" : "⇦"} */}
-            ☰
-          </button>
         </div>
-
-
+      </header>
+      {isCollapsed ? (
+        ""
+      ) : (
         <div className="d-flex header-sidebar-body">
-          {/* Sidebar */}
           <div
-            className={`text-white header-sidebar  ${isCollapsed ? "sidebar-collapsed" : "sidebar-expanded"
-              }`}
+            className={`text-white p-4 header-sidebar  ${
+              isCollapsed ? "sidebar-collapsed" : "sidebar-expanded"
+            }`}
             style={{
               width: isCollapsed ? "0px" : "280px",
               transition: "width  0.3s ease",
             }}
           >
-
-            <div className="p-3">
-              <div className="d-flex justify-content-between  text-info">
-                {/* Back */}
-                <FiX />
-                <FiX size={30} onClick={toggleSidebar}/>
-              </div>
-
-
-              <ul className="list-unstyled">
-                <li className="mb-3">
-                  <a href="#" className="text-decoration-none ">
-                    {!isCollapsed && "Home"}
-                  </a>
-                </li>
-                <li className="mb-3">
-                  <a href="#" className="text-decoration-none ">
-                    {!isCollapsed && "About US"}
-                  </a>
-                </li>
-                <li className="mb-3">
-                  <button
-                    className="btn btn-link text-white text-decoration-none"
-                    onClick={toggleDropdown}
-                    style={{ paddingLeft: 0 }}
-                  >
-                    {!isCollapsed && "Services"}
-                  </button>
-                  {isDropdownOpen && (
-                    <ul className="list-unstyled ms-4">
-                      <li>
-                        <a href="#" className="text-decoration-none text-white">
-                          Project 1
-                        </a>
-                      </li>
-                      <li>
-                        <a href="#" className="text-decoration-none text-white">
-                          Project 2
-                        </a>
-                      </li>
-                    </ul>
-                  )}
-                </li>
-
-                <li className="mb-3">
-                  <a href="#" className="text-decoration-none ">
-                    {!isCollapsed && "Blog"}
-                  </a>
-                </li>
-                <li className="mb-3">
-                  <a href="#" className="text-decoration-none ">
-                    {!isCollapsed && "Contact US"}
-                  </a>
-                </li>
-
-              </ul>
+            <div className="d-flex align-items-center justify-content-between mb-5">
+              <span onClick={()=> toggleSidebar()}>Back</span>
+              <h6>
+                <FiX onClick={()=> toggleSidebar()}/>
+              </h6>
             </div>
+            <ul className="list-unstyled">
+              <li className="mb-3">
+                <Link href={"/"}>Home</Link>
+              </li>
+            </ul>
           </div>
-
-
         </div>
-
-
-      </div>
-
+      )}
     </>
   );
 };
