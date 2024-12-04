9331e889aa70b991a208910a013acfdc41c09007
azan1234567890 <azaan.khalid@techzillaglobal.com>
2024-12-04 19:21:44 +0500

    second banner

---
+++ src/components/Second-Section Box/Second-sec-box.jsx
@@ -0,0 +1,19 @@
+import Para from "../Typography/Para"
+export default function SecondSecBox({SecondBox}) {
+    return(
+        <>
+        {SecondBox.map((items)=>{
+            return(
+          <div className="col-lg-6 col-md-12 mt-4" key={items.id} >
+            <div className="second-sec-box" >
+              <div className="second-sec-box-content d-flex align-items-center ">
+              <div className='circle rounded-circle'></div>
+              <Para title={items.title} className="text-white fs-5" />
+              </div>
+            </div>
+          </div>
+            )
+        })}
+        </>
+    )
+} 
--- src/pages/landing/leadgeneration/index.jsx
+++ src/pages/landing/leadgeneration/index.jsx
@@ -1,31 +1,68 @@
-import Layout from '@/components/layout'
-import React from 'react'
-import "./leadgeneration.css"
+import Layout from "@/components/layout";
+import React from "react";
+import "./leadgeneration.css";
+import H4 from "@/components/Typography/H4";
+import SecondSecBox from "@/components/Second-Section Box/Second-sec-box";
 
 const Index = () => {
+  const SecondBox = [
+    {
+      id: 1,
+      title: "Losing leads due to slow responses.",
+    },
+    {
+      id: 2,
+      title: ["Overwhelmed by too many leads and" ,<br />, "not enough follow-ups."],
+    },
+    {
+      id: 3,
+      title: "Frustrated with outdated sales processes or limited AI tool",
+    },
+    {
+      id: 4,
+      title: "Trouble delivering personalized experiences for prospect.",
+    },
+    {
+      id: 5,
+      title: "Difficulty maintaining quality conversations across time zones.",
+    },
+    {
+      id: 6,
+      title: ["Struggling to scale without skyrocketing ", <br /> ," costs."],
+    },
+  ];
   return (
     <Layout>
-        {/* shahbaz */}
-        <section>
-            <div className='main-wrapper'>
-                <div className='container-fluid'>
-                    ok
-                </div>
+      {/* shahbaz */}
+      <section>
+        <div className="main-wrapper">
+          <div className="container-fluid">ok</div>
+        </div>
+      </section>
+
+      {/* Azan */}
+
+      <section className="second-sec">
+        <div className="second-sec-wrapper">
+          <div className="container">
+            <div className="second-sec-content">
+              <div className="second-sec-heading d-flex justify-content-center">
+              <H4
+                title="Sales can be tough — especially when you're juggling a mountain of leads, customer expectations, and an ever-changing market. If you're feeling the pressure, you're not alone. Many businesses face challenges that slow down growth and drain resources."
+                className="text-white text-capitalize text-center"
+              />
+              </div>
+              <div className="row mt-4 d-flex align-items-center justify-content-center  ">
+                <SecondSecBox SecondBox={SecondBox} />
+              </div>
             </div>
-        </section>
+          </div>
+        </div>
+      </section>
 
-
-        {/* Azan */}
-
-
-
-        {/* anila */}
-
-
-
-
+      {/* anila */}
     </Layout>
-  )
-}
+  );
+};
 
-export default Index
+export default Index;
