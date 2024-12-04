47a91810180d268bd41d99cab49b5ade5320fc75
shahbaz <shahbaznadeem654@gmail.com>
2024-12-04 23:00:59 +0500

    ffinalize banner

--- src/pages/landing/leadgeneration/index.jsx
+++ src/pages/landing/leadgeneration/index.jsx
@@ -5,111 +5,119 @@ import H4 from "@/components/Typography/H4";
 import SecondSecBox from "@/components/Second-Section Box/Second-sec-box";
 import H2 from "@/components/Typography/H2";
 import Para from "@/components/Typography/Para";
-import {FiCheck} from "react-icons/fi"
+import { FiCheck } from "react-icons/fi"
 import H1 from "@/components/Typography/H1";
 import H5 from "@/components/Typography/H5";
 const Index = () => {
-  const SecondBox = [
-    {
-      id: 1,
-      title: "Losing leads due to slow responses.",
-    },
-    {
-      id: 2,
-      title: ["Overwhelmed by too many leads and" ,<br />, "not enough follow-ups."],
-    },
-    {
-      id: 3,
-      title: "Frustrated with outdated sales processes or limited AI tool",
-    },
-    {
-      id: 4,
-      title: "Trouble delivering personalized experiences for prospect.",
-    },
-    {
-      id: 5,
-      title: "Difficulty maintaining quality conversations across time zones.",
-    },
-    {
-      id: 6,
-      title: ["Struggling to scale without skyrocketing ", <br /> ," costs."],
-    },
-  ];
-  return (
-    <Layout>
-      {/* shahbaz */}
-      <section>
+    const SecondBox = [
+        {
+            id: 1,
+            title: "Losing leads due to slow responses.",
+        },
+        {
+            id: 2,
+            title: ["Overwhelmed by too many leads and", <br />, "not enough follow-ups."],
+        },
+        {
+            id: 3,
+            title: "Frustrated with outdated sales processes or limited AI tool",
+        },
+        {
+            id: 4,
+            title: "Trouble delivering personalized experiences for prospect.",
+        },
+        {
+            id: 5,
+            title: "Difficulty maintaining quality conversations across time zones.",
+        },
+        {
+            id: 6,
+            title: ["Struggling to scale without skyrocketing ", <br />, " costs."],
+        },
+    ];
+    return (
+        <Layout>
+            {/* shahbaz */}
+            <section>
                 <div className='main-wrapper text-white'>
                     <div className='container-fluid'>
-                        <img src='/images/4IR-logo-main.png' width={200} />
-                        <div className='row mt-5'>
-                            <div className='col-lg-6'>
-                                <H2 title="Supercharge your Sales Teams with "
+                        <div className='row'>
+                            <div className='col-lg-6 col-md-6 col-sm-12 py-5 colum1'>
+                                <div className="figure1">
+                                    <figure>
+                                        <img src='/images/4IR-logo-main.png' className="bannerImg1" />
+                                    </figure>
+                                </div>
+                                <H2 title="Supercharge Your Sales Teams With "
                                     breakpoint={true}
                                     spanTitle="AI-Driven Solutions."
                                 />
-                                <Para title="Say goodbye to the stress of sales teams and hello to human-like AI that works 24/7." />
-                                <div className='my-5 d-flex justify-content-between'>
-                                    <span className="bannerSpan"><FiCheck className='primary'/> No complex Software</span>
-                                    <span className="bannerSpan"><FiCheck className='primary'/> Easy-integration</span>
-                                    <span className="bannerSpan"><FiCheck className='primary'/> 24/7 Availability</span>
+                                <Para title="Say goodbye to the stress of sales teams and hello to human-like AI that works 24/7." className="py-4 bannerpara" />
+                                <div className='my-3 bannerSpandiv d-flex justify-content-around'>
+                                    <span className="bannerSpan"><FiCheck className='primary' /> No complex Software</span>
+                                    <span className="bannerSpan"><FiCheck className='primary' /> Easy-integration</span>
+                                    <span className="bannerSpan"><FiCheck className='primary' /> 24/7 Availability</span>
                                 </div>
+                                <div className="banner-btn">
 
-                                <a href='#' className='btn-1 px-5 py-3'>Book a free Demo</a>
+                                    <button href='#' className='btn-1 mt-5 fs-2'>Book a free Demo</button>
+                                </div>
                             </div>
-                            <div className='col-lg-6 d-flex justify-content-center align-items-center'>
-                                <figure >
-                                    <img className='bannerImg1' src='/images/man-in-frame.png' alt='lead generation image' width={480} />
-                                </figure>
+                            <div className='col-lg-6 col-md-6 col-sm-12 d-flex justify-content-end align-items-center customScreen'>
+                                <div className="figure2">
+                                    <figure>
+                                        <img className='bannerImg2 figure1img"' src='/images/man-in-frame.png' alt='lead generation image' />
+                                    </figure>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </section>
 
-      {/* Azan */}
+            {/* Azan */}
 
-      <section className="second-sec">
-        <div className="second-sec-wrapper">
-          <div className="container">
-            <div className="second-sec-content">
-              <div className="second-sec-heading d-flex justify-content-center">
-              <H4
-                title="Sales can be tough — especially when you're juggling a mountain of leads, customer expectations, and an ever-changing market. If you're feeling the pressure, you're not alone. Many businesses face challenges that slow down growth and drain resources."
-                className="text-white text-capitalize text-center"
-              />
-              </div>
-              <div className="row mt-4 d-flex align-items-center justify-content-center  ">
-                <SecondSecBox SecondBox={SecondBox} />
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+            <section className="second-sec">
+                <div className="second-sec-wrapper">
+                    <div className="container">
+                        <div className="second-sec-content">
+                            <div className="second-sec-heading d-flex justify-content-center">
+                                <H4
+                                    title="Sales can be tough — especially when you're juggling a mountain of leads, customer expectations, and an ever-changing market. If you're feeling the pressure, you're not alone. Many businesses face challenges that slow down growth and drain resources."
+                                    className="text-white text-capitalize text-center"
+                                />
+                            </div>
+                            <div className="row mt-4 d-flex align-items-center justify-content-center  ">
+                                <SecondSecBox SecondBox={SecondBox} />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
 
-      {/* anila */}
-      <section>
-  <div className="sec-three-wrapper">
-    <div className="container">
-        <div className="sec-three-content align-items-center justify-content-center">
-              <H1 title="introducing"
-              breakpoint={true}
-              spanTitle=" ai talk assist"
-              className="text-uppercase text-white text-center "/>
-              <div>
+            {/* anila */}
+            {/* <section>
+                <div className="sec-three-wrapper">
+                    <div className="container">
+                        <div className="sec-three-content align-items-center justify-content-center">
+                            <H1 title="introducing"
+                                breakpoint={true}
+                                spanTitle=" ai talk assist"
+                                className="text-uppercase text-white text-center " />
+                            <div>
 
-               <H5
-                title="The Revolutionary Voice AI Platform " className={"text-center text-white mt-5 "}/>
+                                <H5
+                                    title="The Revolutionary Voice AI Platform " className={"text-center text-white mt-5 "} />
 
-              </div>
-              <p class="text-center fs-1 mt-5">Transform your sales process in just two days—no long wait times, hassle</p> 
-              
-          </div>
-    </div>
-  </div>
-</section>
-    </Layout>
-  );
+                            </div>
+                            <p class="text-center fs-1 mt-5">Transform your sales process in just two days—no long wait times, hassle</p>
+
+                        </div>
+                    </div>
+                </div>
+            </section> */}
+        </Layout>
+    );
 };
 
 export default Index;
