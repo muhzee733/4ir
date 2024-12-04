dcbecdac4dd8f9aa922e67596570e188d41cd6c6
shahbaz <shahbaznadeem654@gmail.com>
2024-12-04 15:49:55 +0500

    layout done

---
+++ src/pages/landing/leadgeneration/index.jsx
@@ -0,0 +1,24 @@
+import Layout from '@/components/layout'
+import React from 'react'
+
+const Index = () => {
+  return (
+    <Layout>
+        {/* shahbaz */}
+
+
+
+        {/* Azan */}
+
+
+
+        {/* anila */}
+
+
+
+
+    </Layout>
+  )
+}
+
+export default Index
