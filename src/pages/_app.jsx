659642d15dec8977883a55ded515ec5f107d6682
azan1234567890 <azaan.khalid@techzillaglobal.com>
2024-12-05 22:58:56 +0500

    aivoicecalling banner

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,7 +1,10 @@
 import '../app/Global.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { useEffect } from 'react';
 export default function MyApp({ Component, pageProps }) {
+  useEffect(()=>{
+    require("bootstrap/dist/js/bootstrap.bundle.min.js")
+  },[])
   return <Component {...pageProps} />;
  
 }
--- src/pages/landing/leadgeneration/index.jsx
+++ src/pages/landing/leadgeneration/index.jsx
@@ -8,6 +8,8 @@ import H1 from "@/components/Typography/H1";
 import H3 from "@/components/Typography/H3";
 import H5 from "@/components/Typography/H5";
 import H6 from "@/components/Typography/H6";
+import H2 from "@/components/Typography/H2";
+import Accordian from "@/components/Accordian/Accordian";
 // import api from "../../../../public/video/api.mp4"
 
 const Index = () => {
@@ -22,9 +24,7 @@ const Index = () => {
     },
     {
       id: 3,
-      title:
-        "Frustrated with outdated sales processes or limited AI tool"
-
+      title: "Frustrated with outdated sales processes or limited AI tool",
     },
     {
       id: 4,
@@ -32,14 +32,21 @@ const Index = () => {
     },
     {
       id: 5,
-      title:
-        "Difficulty maintaining quality conversations across time zones."
+      title: "Difficulty maintaining quality conversations across time zones.",
     },
     {
       id: 6,
       title: "Struggling to scale without skyrocketing costs.",
     },
   ];
+  // const Accordian=[
+  //   {
+  //     id: 1,
+  //     accordianid: "headingOne",
+
+
+  //   }
+  // ]
   return (
     <Layout>
       {/* shahbaz */}
@@ -50,7 +57,10 @@ const Index = () => {
               <div className="col-lg-6 col-md-6 col-sm-12 colum1">
                 <div className="figure1 d-flex customScreen">
                   <figure>
-                    <img src="/images/4IR-logo-main.png" className="bannerImg1" />
+                    <img
+                      src="/images/4IR-logo-main.png"
+                      className="bannerImg1"
+                    />
                   </figure>
                 </div>
                 <H1
@@ -83,7 +93,7 @@ const Index = () => {
                 <div className="figure2 mt-5">
                   <figure>
                     <img
-                      className='bannerImg2'
+                      className="bannerImg2"
                       src="/images/man-in-frame.png"
                       alt="lead generation image"
                     />
@@ -102,7 +112,8 @@ const Index = () => {
               <div className="section2heading d-flex justify-content-center">
                 <H5
                   title="Sales can be tough — especially when you're juggling a mountain of leads, customer expectations, and an ever-changing market. If you're feeling the pressure, you're not alone. Many businesses face challenges that slow down growth and drain resources."
-                  className="text-capitalize text-center" />
+                  className="text-capitalize text-center"
+                />
               </div>
             </div>
 
@@ -115,9 +126,7 @@ const Index = () => {
                 title="Are these common sales challenges holding your business back?"
                 className="section2h6"
               />
-              <button className="btn-1 ">
-                It’s Time To Make A Change
-              </button>
+              <button className="btn-1 ">It’s Time To Make A Change</button>
             </div>
           </div>
         </div>
@@ -128,8 +137,12 @@ const Index = () => {
           <div className="container">
             <div className="row">
               <div className="col-lg-12 col-md-12 col-sm-12">
-                <div className="section3-span1 text-white text-uppercase text-center">introducing</div>
-                <div className="section3-span1 primary text-uppercase text-center">AI Talk Assist</div>
+                <div className="section3-span1 text-white text-uppercase text-center">
+                  introducing
+                </div>
+                <div className="section3-span1 primary text-uppercase text-center">
+                  AI Talk Assist
+                </div>
                 <div className="section3-main">
                   <H5
                     title="The Revolutionary Voice AI Platform"
@@ -141,6 +154,24 @@ const Index = () => {
                   times, hassle
                 </p>
               </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* Azaan */}
+      <section className="asked-question">
+        <div className="asked-question-wrapper">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-6">
+                <div className="frequently-question-content">
+                  <H2 title="Frequently Asked Question" />
+                  <Para title="Book a Demo if you have any more questions." />
+               <Accordian />
+                </div>
+              </div>
+              <div className="col-lg-6"></div>
             </div>
           </div>
         </div>
