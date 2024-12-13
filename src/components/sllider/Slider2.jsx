cbc642cf68c998913a9cef62edd59f30176c77fa
shahbaz <shahbaznadeem654@gmail.com>
2024-12-13 11:03:49 +0500

    slider changed

--- src/components/sllider/Slider2.jsx
+++ src/components/sllider/Slider2.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
-
+import { Pagination } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
-
 import 'swiper/css';
 import 'swiper/css/pagination';
 
 import './slider.css';
 
-import { Pagination } from 'swiper/modules';
 
 const Slider2 = () => {
     const SliderDataLead = [
@@ -30,6 +28,30 @@ const Slider2 = () => {
     ];
     return (
         <div>
+            {/*  */}
+            {/* <Swiper
+        slidesPerView={'auto'}
+        centeredSlides={true}
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper> */}
+
+
+            {/*  */}
             <Swiper
                 slidesPerView={"auto"}
                 centeredSlides={true}
